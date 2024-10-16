abfb763a4bdc9013bada4478263ac3ffa642db77
Yanya07 <yankabumoney@gmail.com>
2024-10-16 11:41:40 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLine;
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.Button btnSquare;
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.TrackBar trackBarThickness;
+
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLine = new System.Windows.Forms.Button();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.btnSquare = new System.Windows.Forms.Button();
+            this.btnColor = new System.Windows.Forms.Button();
+            this.trackBarThickness = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarThickness)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(205, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(567, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLine
+            // 
+            this.btnLine.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnLine.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnLine.ForeColor = System.Drawing.Color.White;
+            this.btnLine.Location = new System.Drawing.Point(29, 31);
+            this.btnLine.Name = "btnLine";
+            this.btnLine.Size = new System.Drawing.Size(130, 38);
+            this.btnLine.TabIndex = 1;
+            this.btnLine.Text = "Линия";
+            this.btnLine.UseVisualStyleBackColor = false;
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.BackColor = System.Drawing.Color.LightCoral;
+            this.btnCircle.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnCircle.ForeColor = System.Drawing.Color.White;
+            this.btnCircle.Location = new System.Drawing.Point(29, 92);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(130, 36);
+            this.btnCircle.TabIndex = 2;
+            this.btnCircle.Text = "Круг";
+            this.btnCircle.UseVisualStyleBackColor = false;
+            // 
+            // btnSquare
+            // 
+            this.btnSquare.BackColor = System.Drawing.Color.LightGreen;
+            this.btnSquare.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnSquare.ForeColor = System.Drawing.Color.White;
+            this.btnSquare.Location = new System.Drawing.Point(29, 144);
+            this.btnSquare.Name = "btnSquare";
+            this.btnSquare.Size = new System.Drawing.Size(130, 33);
+            this.btnSquare.TabIndex = 3;
+            this.btnSquare.Text = "Квадрат";
+            this.btnSquare.UseVisualStyleBackColor = false;
+            // 
+            // btnColor
+            // 
+            this.btnColor.BackColor = System.Drawing.Color.MediumPurple;
+            this.btnColor.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.btnColor.ForeColor = System.Drawing.Color.White;
+            this.btnColor.Location = new System.Drawing.Point(29, 195);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(130, 34);
+            this.btnColor.TabIndex = 4;
+            this.btnColor.Text = "Цвет";
+            this.btnColor.UseVisualStyleBackColor = false;
+            // 
+            // trackBarThickness
+            // 
+            this.trackBarThickness.BackColor = System.Drawing.Color.White;
+            this.trackBarThickness.Location = new System.Drawing.Point(12, 249);
+            this.trackBarThickness.Minimum = 1;
+            this.trackBarThickness.Name = "trackBarThickness";
+            this.trackBarThickness.Size = new System.Drawing.Size(169, 56);
+            this.trackBarThickness.TabIndex = 5;
+            this.trackBarThickness.Value = 2;
+            // 
+            // Form1
+            // 
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(784, 476);
+            this.Controls.Add(this.trackBarThickness);
+            this.Controls.Add(this.btnColor);
+            this.Controls.Add(this.btnSquare);
+            this.Controls.Add(this.btnCircle);
+            this.Controls.Add(this.btnLine);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Программа для рисования";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarThickness)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Point startPoint;
+        private Point endPoint;
+        private string selectedShape = "Line";
+        private bool isDrawing = false;
+        private Color currentColor = Color.Black;
+        private int lineWidth = 2;
+
+        // Создаем список для хранения нарисованных фигур
+        private List<Shape> shapes = new List<Shape>();
+        private bool isErasing = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Устанавливаем события для рисования
+            pictureBox1.MouseDown += pictureBox1_MouseDown;
+            pictureBox1.MouseMove += pictureBox1_MouseMove;
+            pictureBox1.MouseUp += pictureBox1_MouseUp;
+            pictureBox1.Paint += pictureBox1_Paint; // Событие для перерисовки при каждом обновлении PictureBox
+
+            btnLine.Click += (s, e) =>// Кнопки для выбора формы
+            {
+                selectedShape = "Line";
+                isErasing = false; // Выключаем режим стирателя
+            };
+            btnCircle.Click += (s, e) =>
+            {
+                selectedShape = "Circle";
+                isErasing = false; // Выключаем режим стирателя
+            };
+            btnSquare.Click += (s, e) =>
+            {
+                selectedShape = "Square";
+                isErasing = false; // Выключаем режим стирателя
+            };
+
+            btnColor.Click += (s, e) => ChooseColor();// Кнопка для выбора цвета
+            trackBarThickness.Scroll += (s, e) => lineWidth = trackBarThickness.Value;// Ползунок для выбора толщины
+        }
+
+        
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)// Когда мышь нажата
+        {
+            if (isErasing)
+            {
+                RemoveShapeAt(e.Location);
+            }
+            else
+            {
+                isDrawing = true;
+                startPoint = e.Location;
+            }
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)// Когда мышь перемещается
+        {
+            if (isDrawing)
+            {
+                endPoint = e.Location;
+                pictureBox1.Invalidate(); // Обновляем холст
+            }
+        }
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)// Когда кнопка мыши отпущена
+        {
+            if (isErasing)
+            {
+                return;
+            }
+
+            isDrawing = false;
+            endPoint = e.Location;
+
+            shapes.Add(new Shape// Добавляем новую фигуру в список
+            {
+                StartPoint = startPoint,
+                EndPoint = endPoint,
+                ShapeType = selectedShape,
+                Color = currentColor,
+                LineWidth = lineWidth
+            });
+
+            pictureBox1.Invalidate();// Обновляем PictureBox для перерисовки всех фигур
+        }
+        private void DrawShape(Graphics g, Shape shape) // Рисуем фигуру
+        {
+            Pen pen = new Pen(shape.Color, shape.LineWidth);
+
+            switch (shape.ShapeType)
+            {
+                case "Line":
+                    g.DrawLine(pen, shape.StartPoint, shape.EndPoint);
+                    break;
+                case "Circle":
+                    int radius = Math.Min(Math.Abs(shape.EndPoint.X - shape.StartPoint.X), Math.Abs(shape.EndPoint.Y - shape.StartPoint.Y));
+                    g.DrawEllipse(pen, shape.StartPoint.X, shape.StartPoint.Y, radius, radius);
+                    break;
+                case "Square":
+                    int side = Math.Min(Math.Abs(shape.EndPoint.X - shape.StartPoint.X), Math.Abs(shape.EndPoint.Y - shape.StartPoint.Y));
+                    g.DrawRectangle(pen, shape.StartPoint.X, shape.StartPoint.Y, side, side);
+                    break;
+            }
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e) // Обработчик события Paint для перерисовки всех фигур
+        {
+            Graphics g = e.Graphics;
+
+            foreach (var shape in shapes)// Перерисовываем все фигуры
+            {
+                DrawShape(g, shape);
+            }
+
+            if (isDrawing)// Рисуем текущую фигуру, если мы находимся в процессе рисования
+            {
+                DrawShape(g, new Shape
+                {
+                    StartPoint = startPoint,
+                    EndPoint = endPoint,
+                    ShapeType = selectedShape,
+                    Color = currentColor,
+                    LineWidth = lineWidth
+                });
+            }
+        }
+        private void ChooseColor()// Выбор цвета
+        {
+            using (ColorDialog colorDialog = new ColorDialog())
+            {
+                if (colorDialog.ShowDialog() == DialogResult.OK)
+                {
+                    currentColor = colorDialog.Color;
+                    btnColor.BackColor = currentColor; // Отображаем выбранный цвет на кнопке
+                }
+            }
+        }
+
+        private void RemoveShapeAt(Point location) // Удаление фигуры по месту клика
+        {
+            var remainingShapes = new List<Shape>(); // Создаем временный список для хранения фигур, которые нужно сохранить
+
+            foreach (var shape in shapes)
+            {
+                if (!IsPointInShape(location, shape)) // Проверяем, попадает ли курсор в фигуру
+                {
+                    remainingShapes.Add(shape);
+                }
+            }
+
+            shapes = remainingShapes;// Обновляем список фигур
+            pictureBox1.Invalidate(); // Обновляем PictureBox для перерисовки
+        }
+
+        private bool IsPointInShape(Point point, Shape shape)// Проверка, находится ли точка в фигуре
+        {
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                switch (shape.ShapeType)
+                {
+                    case "Line":
+                        path.AddLine(shape.StartPoint, shape.EndPoint);
+                        break;
+                    case "Circle":
+                        int radius = Math.Min(Math.Abs(shape.EndPoint.X - shape.StartPoint.X), Math.Abs(shape.EndPoint.Y - shape.StartPoint.Y));
+                        path.AddEllipse(shape.StartPoint.X, shape.StartPoint.Y, radius, radius);
+                        break;
+                    case "Square":
+                        int side = Math.Min(Math.Abs(shape.EndPoint.X - shape.StartPoint.X), Math.Abs(shape.EndPoint.Y - shape.StartPoint.Y));
+                        path.AddRectangle(new Rectangle(shape.StartPoint.X, shape.StartPoint.Y, side, side));
+                        break;
+                }
+                return path.IsVisible(point);
+            }
+        }
+                private class Shape // Класс для хранения информации о нарисованных фигурах
+        {
+            public Point StartPoint { get; set; }
+            public Point EndPoint { get; set; }
+            public string ShapeType { get; set; }
+            public Color Color { get; set; }
+            public int LineWidth { get; set; }
+        }
+    }
+}
